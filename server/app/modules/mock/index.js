6e27808042867c6287800bc2546bbe8d57fbe70a
Matheus Almeida <almeida-matheus@hotmail.com.br>
2019-02-09 18:31:51 -0200

    products mock

--- server/app/modules/mock/index.js
+++ server/app/modules/mock/index.js
@@ -1,97 +1,15 @@
-const uuidv1 = require('uuid/v1');
-const randomColor = require('../random-color');
-let participations;
+const products = [];
 
-const setInitialParticipations = () => {
-	if (!(participations instanceof Array)) {
-		participations = [
-			{
-				id: uuidv1(),
-				firstName: 'Marciel',
-				lastName: 'Silva',
-				participation: 5,
-				color: randomColor()
-			},
-			{
-				id: uuidv1(),
-				firstName: 'João',
-				lastName: 'Souza',
-				participation: 5,
-				color: randomColor()
-			},
-			{
-				id: uuidv1(),
-				firstName: 'João',
-				lastName: 'Silva',
-				participation: 5,
-				color: randomColor()
-			},
-			{
-				id: uuidv1(),
-				firstName: 'Maria',
-				lastName: 'Silva',
-				participation: 4,
-				color: randomColor()
-			},
-			{
-				id: uuidv1(),
-				firstName: 'Joaquim',
-				lastName: 'Pereira',
-				participation: 2,
-				color: randomColor()
-			},
-			{
-				id: uuidv1(),
-				firstName: 'Janaina',
-				lastName: 'Ferreira',
-				participation: 1,
-				color: randomColor()
-			},
-			{
-				id: uuidv1(),
-				firstName: 'Janaina',
-				lastName: 'Ferreira',
-				participation: 2,
-				color: randomColor()
-			},
-			{
-				id: uuidv1(),
-				firstName: 'Marcos',
-				lastName: 'Pereira',
-				participation: 2,
-				color: randomColor()
-			},
-			{
-				id: uuidv1(),
-				firstName: 'Márcio',
-				lastName: 'Silva',
-				participation: 2,
-				color: randomColor()
-			},
-			{
-				id: uuidv1(),
-				firstName: 'João',
-				lastName: 'Marciel',
-				participation: 5,
-				color: randomColor()
-			}
-		];
-	}
+module.exports.getProducts = () => products;
+
+module.exports.deleteProduct = () => {
+
 };
 
-module.exports.getParticipations = () => {
-	setInitialParticipations();
-	return participations;
+module.exports.createProduct = () => {
+
 };
 
-module.exports.deleteParticipation = (id) => {
-	setInitialParticipations();
-	participations = participations.filter(item => item.id !== id);
-	return participations;
-};
+module.exports.getProductById = () => {
 
-module.exports.addParticipation = (model) => {
-	setInitialParticipations();
-	participations.unshift(model);
-	return participations;
 };
