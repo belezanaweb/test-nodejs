b38cba312e4196fa3086ac3a0d4e22a3d4a985f2
Erickson Teixeira <rodox.k@hotmail.com>
2019-02-20 17:23:27 -0300

    mongo connection

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express();
-
 const PORT = 5000;
+const mongoURI = 'mongodb://blznaweb:blznaweb1@ds139775.mlab.com:39775/blznaweb';
 
-app.listen(PORT, console.log(`Server running on port: ${PORT}`));
+mongoose.connect(mongoURI, { useNewUrlParser: true })
+    .then(app.listen(PORT, console.log(`Server running on port: ${PORT}`)))
+    .catch(err => console.log(err));
