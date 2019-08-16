d343c4d1d63db3f56d60d5a2ee8395d5cdc00d73
Rafaell Lycan <sonny.webdsg@gmail.com>
2019-08-16 21:27:49 +0200

    Add Product validation service

---
+++ src/services/validations/ProductValidationSchema.ts
@@ -0,0 +1,29 @@
+import joi, { Schema, ValidationError } from 'joi';
+
+import Product from '../../interfaces/Product';
+
+const warehousesSchema: Schema = joi.object().keys({
+  locality: joi.string().required(),
+  quantity: joi.number().required(),
+  type: joi.string().required(),
+});
+
+const inventorySchema: Schema = joi.object().keys({
+  warehouses: joi.array().items(warehousesSchema),
+});
+
+const productSchema: Schema = joi.object().keys({
+  sku: joi.number().required(),
+  name: joi.string().required(),
+  inventory: inventorySchema.required(),
+});
+
+export const validate = (payload: Product): string => {
+  const { error } = joi.validate(payload, productSchema);
+
+  if (error) {
+    const [{ message }] = error.details;
+
+    return message;
+  }
+};
