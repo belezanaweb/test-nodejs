20d1d65a1b2a94c93a670832dd239b69baea21bb
Nauara Melo <nauaramelo@gmail.com>
2020-07-15 01:12:16 -0300

    Criando função responsavel pela atualização do produto

--- src/business/ProductBusiness.ts
+++ src/business/ProductBusiness.ts
@@ -3,6 +3,8 @@ import { Inventory } from '../model/Invetory';
 import { ProductDataBase } from '../data/ProductDataBase';
 import { ConflictError } from '../errors/ConflictError';
 import { InvalidParameterError } from '../errors/InvalidParameterError';
+import { NotFoundError } from '../errors/NotFoundError';
+import { GenericError } from '../errors/GenericError';
 
 export class ProductBusiness {
     constructor(
@@ -15,12 +17,33 @@ export class ProductBusiness {
             throw new InvalidParameterError("Missing Input")
         }
 
-        const productFound = this.productDataBase.findBySku(sku)
-
-        if (productFound) {
+        if (this.hasProductBySku(sku)) {
             throw new ConflictError("The product already exists.")
         }
 
         this.productDataBase.createProduct(new Product(sku, name, inventory))
+    }
+
+    private hasProductBySku(sku: number): boolean {
+        const product = this.productDataBase.findBySku(sku)
+
+        return product ? true : false;
+    }
+
+    public editProduct(skuParams: number, sku: number, name: string, inventory: Inventory): void {
+
+        if (!sku || !name || !inventory) {
+            throw new InvalidParameterError("Missing Input")
+        }
+
+        if (skuParams !== sku) {
+            throw new GenericError("The informed sku does not match the product sku")
+        }
+
+        if (!this.hasProductBySku(skuParams)) {
+            throw new NotFoundError("Product Not Found")
+        }
+        
+        this.productDataBase.editProduct(skuParams, new Product(skuParams, name, inventory))
     }
 }
