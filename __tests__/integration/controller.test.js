03949012ef70f58927c73e99db0739259a071850
skempfer <shana.kempfer88@gmail.com>
2020-08-31 02:02:44 -0300

    feat(test): deleteProduct tested

--- __tests__/integration/controller.test.js
+++ __tests__/integration/controller.test.js
@@ -54,7 +54,7 @@ describe('createProduct', () => {
             .send(products[0]);
 
         expect(responsePost.status).toBe(400);
-    });
+    });    
 
     it('should not be possible to create a product with an empty object', async () => {
         await request(app)
@@ -67,7 +67,86 @@ describe('createProduct', () => {
             expect(400);
     });
 });
-describe('updateProduct',() => {    
+describe('getProduct', () => {
+    it('it must be possible to recover a product by sku', async () => {
+        await request(app)
+            .post('/product')
+            .send(products[0]);
+
+        const response = await request(app)
+            .get(`/product/${products[0].sku}`);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveLength(1);
+    });
+
+    test('the result must not be undefined', async () => {
+        await request(app)
+            .post('/product')
+            .send(products[0]);
+
+        const response = await request(app)
+            .get(`/product/${products[0].sku}`);
+        
+        expect(response.body).toBeDefined();
+    });
+
+    it('must return the exact object that has the requested sku', async () => {
+        await request(app)
+            .post('/product')
+            .send(products[0]);
+
+        const response = await request(app)
+            .get(`/product/${products[0].sku}`);
+
+        expect(response.body).toMatchObject([{'isMarketable': true, "product": {"inventory": {"warehouses": [{"locality": "SP", "quantity": 12, "type": "ECOMMERCE"}]}, "name": "L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium - Máscara de Reconstrução 500g", "sku": 11111}, "totalQuantity": 12}]);
+    });
+
+    it('the isMarketable property must be true if the totalQuantity > 0', async () => {
+        await request(app)
+            .post('/product')
+            .send(products[0]);
+
+        const response = await request(app)
+            .get(`/product/${products[0].sku}`);
+
+        expect(response.body).toMatchObject([{'isMarketable': true}])
+    });
+
+    it('the isMarketable property must be false if the totalQuantity = 0', async () => {
+        await request(app)
+            .post('/product')
+            .send(products[1]);
+
+        const response = await request(app)
+            .get(`/product/${products[1].sku}`);
+
+        expect(response.body).toMatchObject([{'isMarketable': false}]);
+    });
+
+    it('must return the value of totalQuantity != 0', async () => {
+        await request(app)
+            .post('/product')
+            .send(products[0]);
+
+        const response = await request(app)
+            .get(`/product/${products[0].sku}`);
+
+        expect(response.body).toMatchObject([{'totalQuantity': 12}])
+    });
+
+    it('must return the value of totalQuantity === 0', async () => {
+        await request(app)
+            .post('/product')
+            .send(products[1]);
+
+        const response = await request(app)
+            .get(`/product/${products[1].sku}`);
+
+        expect(response.body).toMatchObject([{'totalQuantity': 0}]);
+    });
+});
+describe('updateProduct', () => {    
     it('should be possible to edit a product by sku', async () => {        
        const updatedProduct = {            
            sku: products[0].sku,
@@ -109,4 +188,3 @@ describe('deleteProduct', () => {
             expect(400);        
     });
 });
-
