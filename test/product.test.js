50594e7d2573f29db823eadc8195f6361570c6f5
Marlos Martins <martins.marlos@gmail.com>
2021-03-31 13:37:07 -0300

    Finished

--- test/product.test.js
+++ test/product.test.js
@@ -6,7 +6,10 @@ const Product = require('../action/productActions');
 
 /**Import Mocks */
 const reqCreateProduct = JSON.parse(fs.readFileSync('./test/mocks/reqCreateProduct.json'), 'utf-8');
-const resCreateProduct = JSON.parse(fs.readFileSync('./test/mocks/resCreateProduct.json'), 'utf-8');
+const resCreateProductFailed = JSON.parse(
+  fs.readFileSync('./test/mocks/reqCreateProductFailedValidation.json'),
+  'utf-8',
+);
 const reqProductMock = JSON.parse(fs.readFileSync('./test/mocks/reqProduct.json'), 'utf-8');
 const resProductNotFoundMock = JSON.parse(fs.readFileSync('./test/mocks/resProductNotFound.json'), 'utf-8');
 
@@ -25,4 +28,14 @@ test('get-product', async () => {
 
 test('get-product-not-found', async () => {
   expect(await Product.getProductBySky('99999')).toMatchObject(resProductNotFoundMock);
+});
+
+test('delete-product', async () => {
+  await Product.createProduct(reqCreateProduct);
+  expect(await Product.deleteProductBySky('43264')).toBe('Product (sku): 43264 deleted succesfuly');
+});
+
+test('update-product', async () => {
+  await Product.createProduct(reqCreateProduct);
+  expect(await Product.updateProduct('43264', reqCreateProduct)).toBe('Product (sku): 43264 updated succesfuly');
 });
