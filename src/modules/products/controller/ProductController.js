9c67be51bb78c6f7ae647ed10fdbed561552dd8f
Daniel Makowiesky <dvm1993@hotmail.com>
2021-03-31 23:07:16 -0300

    editar produtos e calcular quantidade

--- src/modules/products/controller/ProductController.js
+++ src/modules/products/controller/ProductController.js
@@ -26,6 +26,17 @@ class ProductController {
 
       const productBySku = products.find((prod) => prod.sku === sku);
 
+      products[0].inventory.quantity = products[0].inventory.warehouses.reduce(
+        (acumulador, elemento) => acumulador + elemento.quantity,
+        0
+      );
+
+      if (products[0].inventory.quantity > 0) {
+        products[0].isMarketable = true;
+      } else {
+        products[0].isMarketable = false;
+      }
+
       if (productBySku) {
         res.status(200).json(productBySku);
       } else {
@@ -40,20 +51,44 @@ class ProductController {
     try {
       const sku = parseInt(req.params.sku);
 
-      if(!sku){
+      if (!sku) {
         res.status(400).end("sku não informado");
       }
       const product = products.filter((prod) => prod.sku === sku)[0];
 
       if (product) {
         products = products.filter((prod) => prod.sku !== sku);
-        res.status(200).send({message: "Produto excluído"})
+        res.status(200).send({ message: "Produto excluído" });
       } else {
         res.status(404).end("sku não encontrado");
       }
-
     } catch (error) {
       res.status(500).json({ err: err.message });
+    }
+  }
+
+  edit(req, res) {
+    try {
+      const skuParam = parseInt(req.params.sku);
+      const product = products.filter((prod) => prod.sku === skuParam)[0];
+
+      if (!product) {
+        res.status(404).send("sku não encontrado");
+      }
+      const { sku, name, inventory } = req.body;
+      if (!name || !inventory) {
+        res
+          .status(400)
+          .end("Verifique se você está passando algum dado no body");
+      }
+
+      product.sku = sku;
+      product.name = name;
+      product.inventory = inventory;
+
+      return res.status(200).send({ message: "Produto editado" });
+    } catch (error) {
+      res.status(500).json({ err: error.message });
     }
   }
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,6 +8,8 @@ const productController = new ProductController()
 routes.post("/products",  productController.create);
 routes.get("/products/:sku", productController.fetch);
 routes.delete("/products/:sku", productController.remove);
+routes.put("/products/:sku", productController.edit);
+
 
 
 
