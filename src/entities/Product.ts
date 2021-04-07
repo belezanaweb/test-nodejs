d6abb725a0382a673e94ce266f0f70c3ddeb128f
Nicole <nzolnier@gmail.com>
2021-04-07 19:59:41 -0300

    product class

--- src/entities/Product.ts
+++ src/entities/Product.ts
@@ -1,15 +1,30 @@
-import { CustomError } from "../error/CustomError";
-
-export class Post {
+export class Product {
 
     constructor(
-        private sku:string,
-        private name:string,
-        private inventory:string[],
-        
-    ) {}
-    
-    public getSku = ():string => this.sku
-    public getName = ():string => this.name
-    public getInventory = ():string[] => this.inventory
+        private sku: string,
+        private name: string,
+        private inventory: Inventory,
+        private isMarketable?:boolean
+    ) { }
+
+    public getSku = (): string => this.sku
+    public getName = (): string => this.name
+    public getInventory = (): Inventory => this.inventory
+    public getIsMarketable = ():boolean | undefined => this.isMarketable
+}
+
+interface Inventory {
+    quantity: string;
+    warehouses: Warehouse[];
+}
+
+interface Warehouse {
+    locality: string;
+    quantity: number;
+    type: WAREHOUSE_TYPE;
+}
+
+enum WAREHOUSE_TYPE {
+    PHYSICAL_STORE = "PHYSICAL_STORE",
+    ECOMMERCE = "ECOMMERCE"
 }
