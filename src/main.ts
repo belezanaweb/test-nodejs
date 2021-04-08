aede05646be76b1c51c83563b6792b32989735e8
Wilterson Garcia <wilterson@peepi.com.br>
2021-04-08 00:59:02 -0300

    Added Swagger OpenAPI

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Products API')
+    .setDescription('This API manages the products')
+    .setVersion('1.0')
+    .addTag('products')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
 
   await app.listen(3000);
 }
