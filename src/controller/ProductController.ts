29d2e4f78ef13257728f8d7ed3b754f7f9614bcf
Matheus <matheus510.fonseca@gmail.com>
2021-04-12 14:11:18 -0300

    adicionando estrutura de router e controller

---
+++ src/controller/ProductController.ts
@@ -0,0 +1,37 @@
+  
+import { Request, Response } from "express";
+
+export class ProductController {
+
+    public createProduct(req: Request, res: Response): void {
+        try {
+
+        } catch (err) {
+
+        }
+    }
+
+    public editProduct(req: Request, res: Response): void {
+        try {
+
+        } catch (err) {
+
+        }
+    }
+
+    public getProduct(req: Request, res: Response): void {
+        try {
+
+        } catch (err) {
+
+        }
+    }
+
+    public deleteProduct(req: Request, res: Response): void {
+        try {
+
+        } catch (err) {
+        
+        }
+    }
+}
---
+++ src/router/ProductRouter.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { ProductController } from "../controller/ProductController";
+
+export const productRouter = express.Router();
+
+productRouter.post("/", new ProductController().createProduct)
+productRouter.put("/:sku", new ProductController().editProduct)
+productRouter.get("/:sku", new ProductController().getProduct)
+productRouter.delete("/:sku", new ProductController().deleteProduct)
