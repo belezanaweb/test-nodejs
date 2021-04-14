3326ed6836d5d9fccef66c0f16ec982bb39f77b0
Matheus <matheus510.fonseca@gmail.com>
2021-04-14 17:35:11 -0300

    rotina de testes de criação de produto criada

--- tests/createProduct.test.ts
+++ tests/createProduct.test.ts
@@ -39,4 +39,102 @@ describe("Testing createProduct", () => {
             expect(err.message).toBe("Invalid input");
         }
     })
+
+    test("Should return 'Invalid input' for empty inventory", () => {
+        expect.assertions(2);
+
+        try {
+            const productBusiness = new ProductBusiness(
+                productDatabase as any
+            );
+            
+            const warehouses: WarehouseInterface[] = [
+                {
+                    locality: 'SP',
+                    quantity: 12,
+                    type: WarehouseType.ECOMMERCE
+                }
+            ]
+
+            const inventory: InventoryInterface = null as any;
+
+            productBusiness.createProduct(
+                43265,
+                "L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium - Máscara de Reconstrução 500g",
+                inventory
+            )
+
+            expect(productBusiness.createProduct).toThrow(Error)
+            expect(productBusiness.createProduct).toThrow('Invalid input')
+        } catch (err) {
+            expect(err.errorCode).toBe(400);
+            expect(err.message).toBe("Invalid input");
+        }
+    })
+
+    test("Should return 'Invalid input' for empty name", () => {
+        expect.assertions(2);
+
+        try {
+            const productBusiness = new ProductBusiness(
+                productDatabase as any
+            );
+            
+            const warehouses: WarehouseInterface[] = [
+                {
+                    locality: 'SP',
+                    quantity: 12,
+                    type: WarehouseType.ECOMMERCE
+                }
+            ]
+
+            const inventory: InventoryInterface = {
+                warehouses
+            }
+
+            productBusiness.createProduct(
+                43265,
+                "",
+                inventory
+            )
+
+            expect(productBusiness.createProduct).toThrow(Error)
+            expect(productBusiness.createProduct).toThrow('Invalid input')
+        } catch (err) {
+            expect(err.errorCode).toBe(400);
+            expect(err.message).toBe("Invalid input");
+        }
+    })
+
+    test("Should create a new product", () => {
+
+        try {
+            const productBusiness = new ProductBusiness(
+                productDatabase as any
+            );
+            
+            const warehouses: WarehouseInterface[] = [
+                {
+                    locality: 'SP',
+                    quantity: 12,
+                    type: WarehouseType.ECOMMERCE
+                }
+            ]
+
+            const inventory: InventoryInterface = {
+                warehouses
+            }
+
+            productBusiness.createProduct(
+                43265,
+                "L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium - Máscara de Reconstrução 500g",
+                inventory
+            )
+            
+            expect(productBusiness.createProduct).toHaveBeenCalledWith(43265);
+            expect(productBusiness.createProduct).toHaveBeenCalledWith("L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium - Máscara de Reconstrução 500g");
+        } catch (err) {
+            expect(err.errorCode).toBeUndefined();
+        }
+    })
 })
