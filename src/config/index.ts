ee2170eafb66bddcdd343154d62694cf715afb3c
Diego <diego.aviz@going2.com.br>
2021-04-19 23:56:40 -0300

    fix: comments

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,5 @@
 import { config } from 'dotenv';
 
-/*
- * Aqui estamos dizendo para o dotenv
- * onde ele deve buscar as variáveis de ambiente
- * NODE_ENV será o stage da nossa aplicação [dev, qa, prod, local, etc...]
- */
 const envfile = `.env.${process.env.NODE_ENV}`;
 const envdir = process.cwd();
 
