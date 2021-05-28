7cc235e73fe50a4ec21eea37331793d43437886c
vitorqueijo <vitor.queijo93@gmail.com>
2021-05-28 04:18:04 -0300

    adding create action

---
+++ src/controllers/actions/createProduct.js
@@ -0,0 +1,52 @@
+module.exports = {
+    async createProduct(req, res) {
+        try{
+            let productToBe = req.body;
+
+            //failsafe
+            if(productToBe == null){
+                throw new NullRequestOrParamsError();
+            }
+
+            let existsSku = await helpers.checkSku(productToBe.sku);
+
+            if(existsSku){
+                throw new SkuReferenceError("Product already inserted. If you're trying to update, use route PUT '/product/update'");
+            }
+
+            let wares = productToBe.inventory.warehouses;
+
+            await knex('products')
+                        .insert({
+                            sku: productToBe.sku,
+                            name: productToBe.name,
+                        });
+
+            for(var i=0; i<wares.length; i++){
+                await knex('warehouses').insert({
+                        locality: wares[i].locality,
+                        quantity: wares[i].quantity,
+                        type: wares[i].type,
+                        product_sku: productToBe.sku
+                    });
+            };
+
+            return res
+                .status(200)
+                .send(`Product '${productToBe.sku}' sucessfully added.`);
+        }
+        catch(error){
+            if(error instanceof SkuReferenceError){
+                return res
+                    .status(400)
+                    .send(error.message);
+            } else if(error instanceof NullRequestOrParamsError) {
+                return res
+                    .status(400)
+                    .send("Body is empty, please re-check your request.");
+            } else {
+                return res.status(500).send("Sorry, it's not you, it's me");
+            }
+        }
+    }
+}
