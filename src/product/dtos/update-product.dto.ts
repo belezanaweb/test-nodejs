c6e4af6f82777d5ba4a54692346b63824706308f
Jaykon Willian de Oliveira <joliveira@rastreabilidadebrasil.com.br>
2021-06-15 14:44:16 -0300

    fix: should not update sku

--- src/product/dtos/update-product.dto.ts
+++ src/product/dtos/update-product.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/swagger';
+import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateProductDto } from './create-product.dto';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(
+  OmitType(CreateProductDto, ['sku']),
+) {}
