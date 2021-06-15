1d455c58219c5f1c1d975ca264f068f57373499c
Jaykon Willian de Oliveira <joliveira@rastreabilidadebrasil.com.br>
2021-06-15 14:17:48 -0300

    test: inject dependencies

--- src/product/product.service.spec.ts
+++ src/product/product.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ProductRepositoryInMemory } from './adapters/product.repository.in-memory';
 import { ProductService } from './product.service';
+import { IProductRepository } from './repository/product.repository.interface';
 
 describe('ProductService', () => {
   let service: ProductService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductService],
+      providers: [
+        ProductService,
+        {
+          provide: IProductRepository,
+          useClass: ProductRepositoryInMemory,
+        },
+      ],
     }).compile();
 
     service = module.get<ProductService>(ProductService);
