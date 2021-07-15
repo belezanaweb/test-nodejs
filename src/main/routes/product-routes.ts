12b46bfaba499c3dcb3f1182bb72c069af8cf1d2
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-15 08:54:20 -0300

    feat: add route to find product by sku

--- src/main/routes/product-routes.ts
+++ src/main/routes/product-routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import { addProductController } from '../../use-cases/implementations/add-product'
 import { editProductController } from '../../use-cases/implementations/edit-product'
+import { findProductController } from '../../use-cases/implementations/find-product'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 
 const productRouter = Router()
 
 productRouter.post('/', adaptRoute(addProductController))
 productRouter.put('/:sku', adaptRoute(editProductController))
+productRouter.get('/:sku', adaptRoute(findProductController))
 
 export { productRouter }
