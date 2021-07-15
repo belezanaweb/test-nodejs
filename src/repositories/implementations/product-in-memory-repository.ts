79ed329287954c1caf351990573545ecda75c87b
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-15 08:21:26 -0300

    feat: add function to calculate product properties

--- src/repositories/implementations/product-in-memory-repository.ts
+++ src/repositories/implementations/product-in-memory-repository.ts
@@ -13,18 +13,27 @@ export class ProductInMemoryRepository implements ICreateProductRepository, IFin
   async create (productData: CreateProductDTO): Promise<ProductModel> {
     const newProduct: ProductModel = productData
     this.products.push(newProduct)
-    return newProduct
+    return this.calculateProperties(newProduct)
   }
 
   async findBySku (sku: number): Promise<ProductModel | undefined> {
     const product = this.products.find(product => product.sku === sku)
-    return product
+    return this.calculateProperties(product)
   }
 
   async update ({ sku, ...data }: UpdateProductDTO): Promise<ProductModel> {
     const findIndex = this.products.findIndex(product => product.sku === sku)
     Object.assign(this.products[findIndex], data)
 
-    return this.products[findIndex]
+    return this.calculateProperties(this.products[findIndex])
+  }
+
+  calculateProperties (product: ProductModel): ProductModel {
+    product.inventory.quantity = product.inventory.warehouses.reduce(
+      (quantity, warehouse) => quantity + warehouse.quantity, 0
+    )
+    product.isMarketable = product.inventory.quantity > 0
+
+    return product
   }
 }
