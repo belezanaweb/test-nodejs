2107b020837ce8a6f1ed4fe3b01ea2409d329f8b
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-15 07:25:27 -0300

    feat: add update product repository

--- src/repositories/implementations/product-in-memory-repository.ts
+++ src/repositories/implementations/product-in-memory-repository.ts
@@ -1,8 +1,9 @@
 import { ProductModel } from '../../domain/models/product'
 import { CreateProductDTO, ICreateProductRepository } from '../create-product'
 import { IFindProductBySkuRepository } from '../find-product-by-sku'
+import { IUpdateProductRepository, UpdateProductDTO } from '../update-product'
 
-export class ProductInMemoryRepository implements ICreateProductRepository, IFindProductBySkuRepository {
+export class ProductInMemoryRepository implements ICreateProductRepository, IFindProductBySkuRepository, IUpdateProductRepository {
   private products: ProductModel[] = []
 
   async all (): Promise<ProductModel[]> {
@@ -18,5 +19,12 @@ export class ProductInMemoryRepository implements ICreateProductRepository, IFin
   async findBySku (sku: number): Promise<ProductModel | undefined> {
     const product = this.products.find(product => product.sku === sku)
     return product
+  }
+
+  async update ({ sku, ...data }: UpdateProductDTO): Promise<ProductModel> {
+    const findIndex = this.products.findIndex(product => product.sku === sku)
+    Object.assign(this.products[findIndex], data)
+
+    return this.products[findIndex]
   }
 }
