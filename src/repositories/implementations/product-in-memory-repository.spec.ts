fb04c4ae3ff48581b1c61c52020ad33de6933fbc
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-15 06:16:12 -0300

    test: add missing test to findBySku

--- src/repositories/implementations/product-in-memory-repository.spec.ts
+++ src/repositories/implementations/product-in-memory-repository.spec.ts
@@ -27,4 +27,37 @@ describe('ProductInMemory Repository', () => {
     const products = await sut.all()
     expect(products).toHaveLength(1)
   })
+
+  test('should return product on findBySku success', async () => {
+    const sut = new ProductInMemoryRepository()
+    await sut.create({
+      sku: 1,
+      name: 'any_name',
+      inventory: {
+        warehouses: [
+          {
+            locality: 'ANY_LOCALITY',
+            quantity: 1,
+            type: 'ANY_TYPE'
+          }
+        ]
+      }
+    })
+
+    const product = await sut.findBySku(1)
+    expect(product).toBeTruthy()
+    expect(product).toMatchObject({
+      sku: 1,
+      name: 'any_name',
+      inventory: {
+        warehouses: [
+          {
+            locality: 'ANY_LOCALITY',
+            quantity: 1,
+            type: 'ANY_TYPE'
+          }
+        ]
+      }
+    })
+  })
 })
