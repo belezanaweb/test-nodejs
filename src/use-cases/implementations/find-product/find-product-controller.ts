7f88ab90362d16d749250aa4b01cb9b66d002d7c
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-15 08:45:14 -0300

    feat: ensure FindProductController returns 500 if findProductUseCase throws

--- src/use-cases/implementations/find-product/find-product-controller.ts
+++ src/use-cases/implementations/find-product/find-product-controller.ts
@@ -1,16 +1,20 @@
 import { IFindProductBySkuUseCase } from '../../../domain/use-cases/find-product-by-sku'
-import { notFound, ok } from '../../../presentation/helpers/http-helper'
+import { notFound, ok, serverError } from '../../../presentation/helpers/http-helper'
 import { IController, IHttpRequest, IHttpResponse } from '../../../presentation/protocols'
 
 export class FindProductController implements IController {
   constructor (private readonly findProductBySkuUseCase: IFindProductBySkuUseCase) {}
 
   async handle (request: IHttpRequest): Promise<IHttpResponse> {
-    const product = await this.findProductBySkuUseCase.execute(request.params.sku)
-    if (product.isLeft()) {
-      return notFound(product.value)
-    }
+    try {
+      const product = await this.findProductBySkuUseCase.execute(request.params.sku)
+      if (product.isLeft()) {
+        return notFound(product.value)
+      }
 
-    return ok(product.value)
+      return ok(product.value)
+    } catch (err) {
+      return serverError('internal')
+    }
   }
 }
