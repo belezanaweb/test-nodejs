3de63021ffa4de76405de390144055377fa2c02a
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-15 06:07:00 -0300

    feat: implement product repository

---
+++ src/repositories/implementations/product-in-memory-repository.ts
@@ -0,0 +1,22 @@
+import { ProductModel } from '../../domain/models/product'
+import { CreateProductDTO, ICreateProductRepository } from '../create-product'
+import { IFindProductBySkuRepository } from '../find-product-by-sku'
+
+export class ProductInMemoryRepository implements ICreateProductRepository, IFindProductBySkuRepository {
+  private products: ProductModel[] = []
+
+  async all (): Promise<ProductModel[]> {
+    return this.products
+  }
+
+  async create (productData: CreateProductDTO): Promise<void> {
+    const newProduct: ProductModel = productData
+    this.products.push(newProduct)
+  }
+
+  async findBySku (sku: number): Promise<ProductModel | undefined> {
+    const product = this.products.find(product => product.sku === sku)
+
+    return product
+  }
+}
