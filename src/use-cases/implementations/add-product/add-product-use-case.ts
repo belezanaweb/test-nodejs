df24dbaf159dd63ef3d3af69d9c9e914eee188cb
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-14 21:59:04 -0300

    feat: ensure AddProductUseCase returns left if FindProductBySku is not undefined

--- src/use-cases/implementations/add-product/add-product-use-case.ts
+++ src/use-cases/implementations/add-product/add-product-use-case.ts
@@ -1,4 +1,4 @@
-import { Either, right } from '../../../core/either'
+import { Either, left, right } from '../../../core/either'
 import { ProductAlreadyExistsError } from '../../../domain/errors/product-already-exists'
 import { ProductModel } from '../../../domain/models/product'
 import { AddProduct, AddProductDTO } from '../../../domain/use-cases/add-product'
@@ -8,7 +8,10 @@ export class AddProductUseCase implements AddProduct {
   constructor (private readonly findProductBySkuRepository: FindProductBySkuRepository) {}
 
   async execute ({ sku, name, warehouses }: AddProductDTO): Promise<Either<ProductAlreadyExistsError, ProductModel>> {
-    await this.findProductBySkuRepository.findBySku(sku)
+    const productAlreadyExists = await this.findProductBySkuRepository.findBySku(sku)
+    if (productAlreadyExists) {
+      return left(new ProductAlreadyExistsError())
+    }
     return new Promise(resolve => resolve(right({} as ProductModel)))
   }
 }
