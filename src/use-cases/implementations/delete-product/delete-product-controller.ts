b7a826bc620ae7ed509212c3f148c34452d76d6e
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-15 09:14:15 -0300

    feat: add controller to delete product

---
+++ src/use-cases/implementations/delete-product/delete-product-controller.ts
@@ -0,0 +1,20 @@
+import { IDeleteProductBySkuUseCase } from '../../../domain/use-cases/delete-product-by-sku'
+import { noContent, notFound, ok, serverError } from '../../../presentation/helpers/http-helper'
+import { IController, IHttpRequest, IHttpResponse } from '../../../presentation/protocols'
+
+export class DeleteProductController implements IController {
+  constructor (private readonly deleteProductBySkuUseCase: IDeleteProductBySkuUseCase) {}
+
+  async handle (request: IHttpRequest): Promise<IHttpResponse> {
+    try {
+      const deleted = await this.deleteProductBySkuUseCase.execute(Number(request.params.sku))
+      if (deleted.isLeft()) {
+        return notFound(deleted.value)
+      }
+
+      return noContent()
+    } catch (err) {
+      return serverError('internal')
+    }
+  }
+}
