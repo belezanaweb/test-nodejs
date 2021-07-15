1948e17ba8c5f94b9cb0fff41496139f4a22caf4
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-15 08:00:02 -0300

    feat: add edit product route

--- src/main/routes/product-routes.ts
+++ src/main/routes/product-routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { addProductController } from '../../use-cases/implementations/add-product'
+import { editProductController } from '../../use-cases/implementations/edit-product'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 
 const productRouter = Router()
 
 productRouter.post('/', adaptRoute(addProductController))
+productRouter.put('/:sku', adaptRoute(editProductController))
 
 export { productRouter }
