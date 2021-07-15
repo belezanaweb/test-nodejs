12a2f581fe07d0d1a37cad93309c0f867d2cbe78
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-14 21:01:24 -0300

    feat: add domain product model

---
+++ src/domain/models/product.ts
@@ -0,0 +1,13 @@
+import { WarehouseModel } from './warehouse'
+
+export type ProductModel = {
+  sku: number
+  name: string
+  inventory: InventoryModel
+  isMarketable: boolean
+}
+
+export type InventoryModel = {
+  quantity: number
+  warehouses: WarehouseModel[]
+}
---
+++ src/domain/models/warehouse.ts
@@ -0,0 +1,5 @@
+export type WarehouseModel = {
+  locality: string
+  quantity: number
+  type: string
+}
