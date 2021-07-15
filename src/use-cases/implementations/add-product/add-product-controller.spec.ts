41f82dbcdf1e55441e522425480124ebf8c11a41
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-14 22:48:57 -0300

    refactor: move sut creation to helper method

--- src/use-cases/implementations/add-product/add-product-controller.spec.ts
+++ src/use-cases/implementations/add-product/add-product-controller.spec.ts
@@ -1,38 +1,61 @@
 import { Either, right } from '../../../core/either'
 import { ProductAlreadyExistsError } from '../../../domain/errors/product-already-exists'
 import { AddProduct, AddProductDTO, CreatedProduct } from '../../../domain/use-cases/add-product'
+import { IController } from '../../../presentation/protocols/controller'
+import { IHttpRequest } from '../../../presentation/protocols/http'
 import { AddProductController } from './add-product-controller'
+
+const makeAddProductUseCase = (): AddProduct => {
+  class AddProductUseCaseStub implements AddProduct {
+    async execute ({ sku, name, warehouses }: AddProductDTO): Promise<Either<ProductAlreadyExistsError, CreatedProduct>> {
+      return new Promise(resolve => resolve(right({} as CreatedProduct)))
+    }
+  }
+  return new AddProductUseCaseStub()
+}
+
+const makeFakeSutRequest = (): IHttpRequest => ({
+  body: {
+    sku: 1,
+    name: 'any_name',
+    warehouses: [{
+      locality: 'any_locality',
+      quantity: 1,
+      type: 'any_type'
+    }]
+  }
+})
+
+const makeFakeAddProductDTO = (): AddProductDTO => ({
+  sku: 1,
+  name: 'any_name',
+  warehouses: [{
+    locality: 'ANY_LOCALITY',
+    quantity: 1,
+    type: 'ANY_TYPE'
+  }]
+})
+
+type SutTypes = {
+  addProductUseCaseStub: AddProduct
+  sut: IController
+}
+const makeSut = (): SutTypes => {
+  const addProductUseCaseStub = makeAddProductUseCase()
+  const sut = new AddProductController(addProductUseCaseStub)
+
+  return {
+    addProductUseCaseStub,
+    sut
+  }
+}
 
 describe('AddProduct Controller', () => {
   test('should call AddProductUseCase with correct params', async () => {
-    class AddProductUseCaseStub implements AddProduct {
-      async execute ({ sku, name, warehouses }: AddProductDTO): Promise<Either<ProductAlreadyExistsError, CreatedProduct>> {
-        return new Promise(resolve => resolve(right({} as CreatedProduct)))
-      }
-    }
-    const addProductUseCaseStub = new AddProductUseCaseStub()
-    const sut = new AddProductController(addProductUseCaseStub)
+    const { sut,addProductUseCaseStub } = makeSut()
     const executeSpy = jest.spyOn(addProductUseCaseStub, 'execute')
-    await sut.handle({
-      body: {
-        sku: 1,
-        name: 'any_name',
-        warehouses: [{
-          locality: 'any_locality',
-          quantity: 1,
-          type: 'any_type'
-        }]
-      }
-    })
+    await sut.handle(makeFakeSutRequest())
 
-    expect(executeSpy).toHaveBeenCalledWith({
-      sku: 1,
-      name: 'any_name',
-      warehouses: [{
-        locality: 'ANY_LOCALITY',
-        quantity: 1,
-        type: 'ANY_TYPE'
-      }]
-    })
+    expect(executeSpy).toHaveBeenCalledWith(makeFakeAddProductDTO())
   })
 })
