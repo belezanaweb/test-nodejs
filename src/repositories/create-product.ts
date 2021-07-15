042dbafbe0748c55d6b328a564b33764b28c2925
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-14 22:09:27 -0300

    feat: ensure AddProductUseCase calls CreateProductRepository with correct params

---
+++ src/repositories/create-product.ts
@@ -0,0 +1,13 @@
+import { WarehouseModel } from '../domain/models/warehouse'
+
+export type CreateProductDTO = {
+  sku: number
+  name: string
+  inventory: {
+    warehouses: WarehouseModel[]
+  }
+}
+
+export interface ICreateProductRepository {
+  create(productDTO: CreateProductDTO): Promise<void>
+}
--- src/use-cases/implementations/add-product/add-product-use-case.ts
+++ src/use-cases/implementations/add-product/add-product-use-case.ts
@@ -2,16 +2,23 @@ import { Either, left, right } from '../../../core/either'
 import { ProductAlreadyExistsError } from '../../../domain/errors/product-already-exists'
 import { ProductModel } from '../../../domain/models/product'
 import { AddProduct, AddProductDTO } from '../../../domain/use-cases/add-product'
+import { ICreateProductRepository } from '../../../repositories/create-product'
 import { FindProductBySkuRepository } from '../../../repositories/find-product-by-sku'
 
 export class AddProductUseCase implements AddProduct {
-  constructor (private readonly findProductBySkuRepository: FindProductBySkuRepository) {}
+  constructor (
+    private readonly findProductBySkuRepository: FindProductBySkuRepository,
+    private readonly createProductRepository: ICreateProductRepository
+  ) {}
 
   async execute ({ sku, name, warehouses }: AddProductDTO): Promise<Either<ProductAlreadyExistsError, ProductModel>> {
     const productAlreadyExists = await this.findProductBySkuRepository.findBySku(sku)
     if (productAlreadyExists) {
       return left(new ProductAlreadyExistsError())
     }
+
+    await this.createProductRepository.create({ sku, name, inventory: { warehouses } })
+
     return new Promise(resolve => resolve(right({} as ProductModel)))
   }
 }
