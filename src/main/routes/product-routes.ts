58d972dbd02fe04dfe6910ccbb74cdd201b0bd60
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2021-07-15 09:17:58 -0300

    feat: add route to delete product by sku

--- src/main/routes/product-routes.ts
+++ src/main/routes/product-routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { addProductController } from '../../use-cases/implementations/add-product'
+import { deleteProductController } from '../../use-cases/implementations/delete-product'
 import { editProductController } from '../../use-cases/implementations/edit-product'
 import { findProductController } from '../../use-cases/implementations/find-product'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
@@ -9,5 +10,6 @@ const productRouter = Router()
 productRouter.post('/', adaptRoute(addProductController))
 productRouter.put('/:sku', adaptRoute(editProductController))
 productRouter.get('/:sku', adaptRoute(findProductController))
+productRouter.delete('/:sku', adaptRoute(deleteProductController))
 
 export { productRouter }
