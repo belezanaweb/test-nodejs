613c78a1b1609e44df70303d7482940133eb73a5
Gio Cruz <giocruz.it@gmail.com>
2023-04-13 01:12:11 -0300

    test: 💍 adding unit tests for entities

---
+++ __tests__/entities/Inventory.test.ts
@@ -0,0 +1,25 @@
+import Inventory from "src/entities/Inventory";
+import Warehouse, { WarehouseType } from "src/entities/Warehouse";
+
+describe("[Entities] Inventory", () => {
+  const warehouses = [
+    new Warehouse({ locality: "SP", type: WarehouseType.ecommerce, quantity: 10 }),
+    new Warehouse({ locality: "MOEMA", type: WarehouseType.physicalStore, quantity: 5 }),
+    new Warehouse({ locality: "TAUBATÉ", type: WarehouseType.ecommerce, quantity: 3 }),
+  ];
+
+  const inventory = new Inventory({ warehouses });
+
+  it("should return the correct warehouses", () => {
+    expect(inventory.getWarehouses()).toEqual(warehouses);
+  });
+
+  it("should return the correct quantity", () => {
+    expect(inventory.getQuantity()).toBe(18);
+  });
+
+  it("should return 0 quantity if there are no warehouses", () => {
+    const emptyInventory = new Inventory({ warehouses: [] });
+    expect(emptyInventory.getQuantity()).toBe(0);
+  });
+});
---
+++ __tests__/entities/Product.test.ts
@@ -0,0 +1,31 @@
+import Inventory from "src/entities/Inventory";
+import Product from "src/entities/Product";
+
+describe("[Entities] Product", () => {
+  const inventory = new Inventory({ warehouses: [] });
+  const product = new Product({ sku: "123", name: "Test Product", inventory });
+
+  it("should return the correct SKU", () => {
+    expect(product.getSku()).toBe("123");
+  });
+
+  it("should return the correct name", () => {
+    expect(product.getName()).toBe("Test Product");
+  });
+
+  it("should return the correct inventory", () => {
+    expect(product.getInventory()).toBe(inventory);
+  });
+
+  describe("getIsMarketable", () => {
+    it("should return false if quantity is 0", () => {
+      jest.spyOn(inventory, "getQuantity").mockReturnValueOnce(0);
+      expect(product.getIsMarketable()).toBe(false);
+    });
+
+    it("should return true if quantity is greater than 0", () => {
+      jest.spyOn(inventory, "getQuantity").mockReturnValueOnce(1);
+      expect(product.getIsMarketable()).toBe(true);
+    });
+  });
+});
