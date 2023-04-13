df62ed76c5e1a74fcb05c522096e37228238bc2d
Gio Cruz <giocruz.it@gmail.com>
2023-04-13 01:04:25 -0300

    feat: 🎸 adding entities

---
+++ src/entities/Product.ts
@@ -0,0 +1,26 @@
+import Inventory from "./Inventory";
+
+export default class Product {
+  private sku: string;
+  private name: string;
+  private inventory: Inventory;
+  private isMarketable: boolean;
+
+  constructor({ sku, name, inventory }: { sku: string, name: string, inventory: Inventory }) {
+    this.sku = sku;
+    this.name = name;
+    this.inventory = inventory;
+    this.isMarketable = false;
+  }
+
+  public getSku(): string { return this.sku }
+  public getName(): string { return this.name }
+  public getInventory(): Inventory { return this.inventory }
+  public getIsMarketable(): boolean {
+    this.isMarketable = false
+    if (this.inventory.getQuantity() > 0) {
+      this.isMarketable = true;
+    }
+    return this.isMarketable;
+  }
+}
