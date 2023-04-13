1865adafac895f705c7d7b0a625bf962dd3fb27f
Gio Cruz <giocruz.it@gmail.com>
2023-04-13 00:20:17 -0300

    feat: 🎸 adding absolute imports

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    'src/(.*)': '<rootDir>/src/$1',
+    'tests/(.*)': '<rootDir>/__tests__/$1',
+  }
 };
