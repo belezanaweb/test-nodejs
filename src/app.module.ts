aa14b2f482ae0eaa6db0233a1fd443fde5de85c6
Lucas Marchiori <sinonsrt@hotmail.com>
2023-05-16 16:22:45 -0300

    configure mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://admin:admin@localhost:27017/belezanaweb?authSource=admin',
+    ),
+    ProductModule,
+  ],
   controllers: [],
   providers: [],
 })
