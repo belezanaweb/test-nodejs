a2205c217a260133163492e0a04512ae9b518e9e
Alini <alini@x>
2023-07-17 23:44:22 -0300

    feat: creates tests, index for the x> Soval

--- src/errors/InvalidError.ts
+++
@@ -1,7 +0,0 @@
-import ApplicationError from "./ApplicationError";
-
-export default class InvalidError extends ApplicationError {
-  constructor(message = "Invalid data") {
-    super(message);
-  }
-}
--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,6 +1,5 @@
 import ApplicationError from "./ApplicationError";
 import DuplicateError from "./DuplicateError";
-import InvalidError from "./InvalidError";
 import ProductNotFoundError from "./ProductNotFoundError";
 
-export { ApplicationError, DuplicateError, InvalidError, ProductNotFoundError };
+export { ApplicationError, DuplicateError, ProductNotFoundError };
