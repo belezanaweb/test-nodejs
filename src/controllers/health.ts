7b41d4b2440926e265e9c0b0b02dd7d62d43a689
rafamagalhas <magalhas.rafa@gmail.com>
2023-08-10 14:38:15 -0300

    refactor: health route resource

--- src/controllers/health.ts
+++ src/controllers/health.ts
@@ -1,15 +1,17 @@
-import { Request, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
-import { healthcheck } from '../business/health';
+import { NextFunction } from "express";
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import { healthcheck } from "../business/health";
 
-async function health(_req: Request, res: Response): Promise<Response> {
+export async function health(
+  _req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<Response> {
   try {
     const response = healthcheck();
     return res.status(StatusCodes.OK).json(response);
   } catch (error) {
-    console.log(error);
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message: error.message });
+    next(error);
   }
 }
-
-export default health;
