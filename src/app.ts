7315f25f672fbf89384d497ecee38924c8bbf38e
rafamagalhas <magalhas.rafa@gmail.com>
2023-08-10 14:36:58 -0300

    feat: add error handler in app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import express from 'express';
+import { StatusCodes } from 'http-status-codes';
+import express, { Request, Response, NextFunction } from 'express';
 import morgan from 'morgan';
 import { routes } from './routes';
+import { logger } from './utils/winston';
 
 const app = express();
 
@@ -8,5 +10,10 @@ app.use(express.json());
 app.use(morgan('tiny'));
 
 app.use('/api/v1', routes);
+
+app.use((error: Error, _req: Request, res: Response, _next: NextFunction) => {
+  logger.error(error);
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
+})
 
 export default app;
